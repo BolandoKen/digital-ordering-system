c905944cacd5ee171e13a875668402fef359f930
Ken Bolando <163643804+BolandoKen@users.noreply.github.com>
2025-04-09 13:15:06 +0800

    enhanced QButton1 class with customizable properties

--- src/components/Buttons.py
+++ src/components/Buttons.py
@@ -1,3 +1,4 @@
+import sys
 from PyQt6.QtWidgets import (
     QApplication,
     QVBoxLayout,
@@ -11,11 +12,27 @@ from PyQt6.QtWidgets import (
     QLineEdit,
     QFileDialog
 )
-
+from PyQt6.QtGui import QIcon
+from PyQt6.QtWidgets import QPushButton
 
 class QButton1(QPushButton) :
-    def __init__(self, text) :
-        super().__init__(text)
-        self.setStyleSheet("background-color: black; color: white")
+    def __init__(self, text, width, height, color, text_color, icon=None, font_style="Regular", font_size=20):
+        super().__init__(text if text else "")
+        self.setFixedSize(width, height)
+        self.setStyleSheet(f"""
+                           background-color: {color};
+                           color: {text_color};
+                           border-radius: 10px;
+                           font-family: 'Helvietica';
+                           font-size: {font_size}px;
+                           font-weight: {font_style};
+                           """)
+        self.clicked.connect(self.on_click)
 
+        if icon: # if an icon is provided
+            self.setIcon(QIcon(icon))
+            self.setIconSize(self.size())
 
+    def on_click(self):
+        print(f"Button clicked: {self.text()}")
+       
