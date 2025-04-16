163ec95076e567d05f0293403578774d8c4965c7
Ken Bolando <163643804+BolandoKen@users.noreply.github.com>
2025-04-16 19:08:37 +0800

    Add Dine In and Take Out buttons including icons, update styles and layout

--- src/components/0kin.py
+++ src/components/0kin.py
@@ -10,7 +10,7 @@ from PyQt6.QtWidgets import (
     QFrame,
 )
 from PyQt6.QtCore import Qt
-from src.components.Buttons import QDeleteButton, QBackButton, QLogoButton, QPrimaryButton, QSecondaryButton
+from src.components.Buttons import QDeleteButton, QBackButton, QLogoButton, QPrimaryButton, QSecondaryButton, QDineInButton, QTakeOutButton
 
 class QWindow(QMainWindow):
     def __init__(self):
@@ -22,15 +22,20 @@ class QWindow(QMainWindow):
         delete_button = QDeleteButton()
         back_button = QBackButton()
         # logo_button = QLogoButton("assets/icons/Logo.png", "M'sKitchen")
+        dineIn_button = QDineInButton()
+        # takeOut_button = QTakeOutButton()
 
         main_layout.addWidget(delete_button)
         main_layout.addWidget(back_button)
         # main_layout.addWidget(logo_button)
+        main_layout.addWidget(dineIn_button)
+        # main_layout.addWidget(takeOut_button)
+
         main_layout.addWidget(QPrimaryButton("hi primary", 400))
         main_layout.addWidget(QSecondaryButton("hi secondary", 400))
         main_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
         main_centralwidget = QWidget()
-        main_centralwidget.setStyleSheet("background: white;")
+        main_centralwidget.setStyleSheet("background: #C8161D;")
         main_centralwidget.setLayout(main_layout)
         self.setCentralWidget(main_centralwidget)
         
