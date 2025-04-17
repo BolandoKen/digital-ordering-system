bce603e5c79b80276aec6fd6f53a9a58f9dfd0a5
Rcjot <rcappy495@gmail.com>
2025-04-17 11:29:06 +0800

    placed catcombobox in stats panel, not yet functional

--- src/components/ComboBox.py
+++ src/components/ComboBox.py
@@ -15,13 +15,15 @@ from PyQt6.QtWidgets import (
 from src.database.queries import fetchCatList
 
 class QCatComboBox(QComboBox) :
-    def __init__(self):
+    def __init__(self, typeOf = None):
         super().__init__()
         self.catList = fetchCatList("admin") 
-
+        if typeOf == "stat" :
+            self.addItem("no filter")
         for cat in self.catList : 
             self.addItem(cat[1], cat[0])
-        
+        # this should listen to any category updates (not yet implemented)
+
     def setDefaultOption(self, cat_id) :
         for i in range(self.count()) :
             if int(self.itemData(i)) == int(cat_id) :
--- src/panels/statsPanel/StatsPanel.py
+++ src/panels/statsPanel/StatsPanel.py
@@ -19,6 +19,7 @@ from PyQt6.QtCore import Qt
 from src.database.queries import fetchStatistics
 from src.components.Headers import QOtherPanelHeader
 from src.components.Table import QStatsTable
+from src.components.ComboBox import QCatComboBox
 
 class QStatsPanel(QFrame) :
     def __init__(self):
@@ -27,9 +28,9 @@ class QStatsPanel(QFrame) :
         self.stats_layout.setContentsMargins(0,0,0,0)
         self.stats_layout.setSpacing(0)
         self.mostordered = True
-        self.page_setup()
+        self.stats_setup()
 
-    def page_setup(self):
+    def stats_setup(self):
    
         self.stats_layout.addWidget(QOtherPanelHeader("Statistics"))
         queryBarHLayout = QHBoxLayout()
@@ -37,12 +38,17 @@ class QStatsPanel(QFrame) :
         self.sort_btn.clicked.connect(self.changeorder)
         queryBarHLayout.addWidget(self.sort_btn)
         queryBarHLayout.addStretch()
-        queryBarHLayout.addWidget(QPushButton("filter"))
+        catComboBox = QCatComboBox("stat")
+        queryBarHLayout.addWidget(catComboBox)
         contentsVLayout = QVBoxLayout()
         contentsVLayout.setContentsMargins(10,10,0,10)
         contentsVLayout.setSpacing(5)
 
-        self.table = QStatsTable()
+        self.table = QStatsTable() 
+        # on combobox change update stats table, make function inside to handle it
+        # check edit dialog, access category_id by self.categoryidComboBox.itemData(self.categoryidComboBox.currentIndex())
+        # combobox has displayed value (category name) and inner value (category id)
+        # we take inner value to query into sql
         contentsVLayout.addLayout(queryBarHLayout)
         contentsVLayout.addWidget(self.table)
         self.stats_layout.addLayout(contentsVLayout)
@@ -55,5 +61,3 @@ class QStatsPanel(QFrame) :
             self.sort_btn.setText("Show Most Ordered")
         
         self.table.setOrderBy(self.mostordered)
-
-    #Still have to make handleSubmitOrderClicked actually send the data into the database (orderitems is empty)
