ac820f94994cfdc4bd521e16c4224910c9bd0f04
Rcjot <rcappy495@gmail.com>
2025-05-23 23:06:30 +0800

    add dotenv lol

--- src/database/init_db.py
+++ src/database/init_db.py
@@ -1,10 +1,13 @@
+from dotenv import load_dotenv
+import os
 import mysql.connector
+load_dotenv()
 
 db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="root",
-    database="orderoo_db" 
+    host=os.getenv("DB_HOST"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASS"),
+    database=os.getenv("DB_NAME") 
 )
 db.autocommit = True
 
